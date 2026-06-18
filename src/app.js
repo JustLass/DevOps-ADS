6e5dbdc3eb4fcd5792746e8a1fe39e5de7e75cb4
erio7 <ericamorim3405@gmail.com>
2026-06-18 19:11:48 -0300

    Atualiza mensagem da aplicacao para apresentacao

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const appInfo = {
   name: "DevOps ADS",
   version: "1.0.0",
   status: "ok",
-  message: "Aplicação Node.js rodando para Demo em aula",
+  message: "Aplicação Rodando para Demonstração em Aula",
 };
 
 function getAppInfo() {
